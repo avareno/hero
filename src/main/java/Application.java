2dfb28439b2c73e72e11921e4fc63feefc238059
avareno <miguelcabral798@gmail.com>
2023-09-27 17:35:26 +0100

    Create Hero class

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,10 @@
+import java.io.IOException;
+
 public class Application {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        Game f = new Game();
+        f.run();
+
+
     }
 }
--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,27 +1,72 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
+
 import java.io.IOException;
 
 public class Game
 {
+    private static Screen screen;
+    private int x = 10;
+    private int y = 10;
     private void draw() throws IOException{
-
+        screen.clear();
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
+        screen.refresh();
     }
+    private void processKey(KeyStroke key) throws IOException {
 
+
+        switch(key.getKeyType())
+        {
+            case ArrowLeft:
+                x--;
+                draw();
+                break;
+            case ArrowDown:
+                y++;
+                draw();
+                break;
+            case ArrowRight:
+                x++;
+                draw();
+                break;
+            case ArrowUp:
+                y--;
+                draw();
+                break;
+        }
+        System.out.println(key);
+    }
     public void run() throws IOException {
         draw();
+        KeyStroke key = screen.readInput();
+        processKey(key);
     }
-    public Game()
+
+
+
+
+    Game()
     {
         try {
-            Terminal terminal = new
-                    DefaultTerminalFactory().createTerminal();
-            Screen screen = new TerminalScreen(terminal);
+
+            TerminalSize terminalSize = new TerminalSize(40, 800);
+            DefaultTerminalFactory terminalFactory = new
+                    DefaultTerminalFactory()
+                    .setInitialTerminalSize(terminalSize);
+            Terminal terminal = terminalFactory.createTerminal();
+            screen = new TerminalScreen(terminal);
             screen.setCursorPosition(null); // we don't need a cursor
             screen.startScreen(); // screens must be started
             screen.doResizeIfNecessary(); // resize screen if necessary
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
